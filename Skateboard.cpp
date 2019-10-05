e3d044f5ea1bc17e968275cc545ed0e475d0688f
Guillermo Villegas <gev14b@my.fsu.edu>
2019-10-04 20:55:08 -0400

    Skateboard class finished
    Class finished with it's randomly generated mileage.

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -1,5 +1,9 @@
 #include "Skateboard.h"
-
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <random>
 
 Skateboard::Skateboard(string brand, string model, int gearCount) {
     setBrand(brand);
@@ -17,9 +21,21 @@ void Skateboard::setGearCount(int gearCount) {
     myGearCount = gearCount;
 }
 
-double Skateboard::mileageEstimate(double time) {
-    double mileage = 3 * time;
-    mileage += mileage * (myGearCount * 0.1);
+double Skateboard::mileageEstimate(double t) {
+    srand(time(NULL));
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_real_distribution<double> distribution(0.1,0.5);
+    double rando = distribution(generator);
+
+    double mileage = rando * t;
+    if(t>25 && t<250){
+      std::random_device rd;
+      std::default_random_engine generator(rd());
+      std::uniform_real_distribution<double> distribution(1,t/3);
+      double rando2 = distribution(generator);
+      mileage += rando2;
+    }
     return mileage;
 }
 
